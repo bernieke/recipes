c7e78cc5a978037509e0bc750b0bcfd1b8ad8c4e
Bernard Kerckenaere <bernieke@bernieke.com>
2019-06-06 19:21:37 +0200

    handle ingredients with removed units

--- recipes/migrations/0003_remove_units.py
+++ recipes/migrations/0003_remove_units.py
@@ -10,12 +10,16 @@ UNITS = [
 
 def remove_units(apps, schema_editor):
     Unit = apps.get_model('recipes', 'Unit')
+    Ingredient = apps.get_model('recipes', 'Ingredient')
 
     for measured, name in UNITS:
         try:
-            Unit.objects.get(name=name, measured=measured).delete()
-        except Unit.DoesNotExist:
-            pass
+            unit_from = Unit.objects.get(name=name, measured=measured)
+            unit_to = Unit.objects.get(name=name, measured='V')
+        except:
+            continue
+        Ingredient.objects.filter(unit=unit_from).update(unit=unit_to)
+        unit_from.delete()
 
 
 class Migration(migrations.Migration):
