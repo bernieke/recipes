0a83f887fa089cbe9bf7c44572e4513ecfeadff0
Bernard Kerckenaere <bernieke@bernieke.com>
2019-08-01 21:45:12 +0200

    handle deleted unit

--- recipes/migrations/0004_rename_floz.py
+++ recipes/migrations/0004_rename_floz.py
@@ -4,9 +4,12 @@ from django.db import migrations
 def rename_floz(apps, schema_editor):
     Unit = apps.get_model('recipes', 'Unit')
 
-    floz = Unit.objects.get(name='oz', measured='V')
-    floz.name = 'fl oz'
-    floz.save()
+    try:
+        floz = Unit.objects.get(name='oz', measured='V')
+        floz.name = 'fl oz'
+        floz.save()
+    except Unit.DoesNotExist:
+        pass
 
 
 class Migration(migrations.Migration):
