8b894a87a359bc2d1565af7568a7039ffd1e64cb
Bernard Kerckenaere <bernieke@bernieke.com>
2020-04-22 12:14:18 +0200

    fix publish_latest_tag.sh to use git instead of mercurial

--- publish_latest_tag.sh
+++ publish_latest_tag.sh
@@ -4,7 +4,7 @@ docker image rm recipes:latest
 docker build . -t recipes --force-rm
 
 IMAGE_ID=`docker image ls | grep recipes | grep latest | awk '{print $3}'`
-TAG=`hg tags | grep -v tip | head -n 1 | awk '{print $1}'`
+TAG=`git tag | tail -n 1`
 
 docker tag $IMAGE_ID bernieke/recipes:$TAG
 docker push bernieke/recipes:$TAG
